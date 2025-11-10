72d2a251104fc5e5db267420da5d1120bee7e4b4
006Julio <165156027+006Julio@users.noreply.github.com>
2025-11-10 16:00:22 -0500

    Update ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,13 +1,19 @@
 
+import java.util.Scanner;
+
+
 public class ejercicio2 {
     public static void main(String[]args){
         /**
          * Crear un algoritmo que multiplique 2 números sin usar el operador de multiplicación. Por ejemplo:
          */
-        int a = 7 ;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el primer número de la mulptiplación: ");
+        int a = sc.nextInt();
+        System.out.println("Ingresa el primer número de la mulptiplación: ");
+        int b = sc.nextInt();
         int multiplicacion = 0;
-        for (int i = 0; i<b; i++){
+        for (int i = 0; i< b; i++){
             multiplicacion += a ;
         }
         System.out.println("la multplicacione sde "+a+" * "+b+" = "+multiplicacion );
